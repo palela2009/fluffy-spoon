ad8078b93d0cfccaf2abd8763882a877506c6b33
palela2009 <alexandre26062009@gmail.com>
2025-04-08 22:24:06 +0400

    All Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.scss"
 import { Route, Routes } from "react-router"
 import { HomePage } from "./pages/Home"
 import { AuthPage } from "./pages/AuthPage"
+import { Dashboard } from "./dashboard/dashboard"
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/auth" element={<AuthPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }

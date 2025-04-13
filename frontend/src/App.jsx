2e39798fbfb4a8d41ff5abb74daf4655eb7a06fe
palela2009 <alexandre26062009@gmail.com>
2025-04-13 14:27:58 +0400

    add dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.scss"
 import { Route, Routes } from "react-router"
 import { HomePage } from "./pages/Home"
+import { Dashboard } from "./dashboard/dashboard"
+
+
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }

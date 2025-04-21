af804f3abfc4138fe93bcd64176fe5365ec24750
palela2009 <alexandre26062009@gmail.com>
2025-04-21 21:17:28 +0400

    all done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss"
 import { Route, Routes } from "react-router"
 import { HomePage } from "./pages/Home"
+import { Userpage } from "./Userpage/userpage"
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="/userpage" element={<Userpage />} />
     </Routes>
   )
 }
